2ddc7db540d9412b6a679464eaf8bfd91e446926
YPerezM <yperezmartinez@danielcastelao.org>
2015-12-11 13:34:08 +0100

    Creacion archivo xml en res-layout-values llamado config.xml para definir la variable bool

--- app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemDetailFragment.java
+++ app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemDetailFragment.java
@@ -84,4 +84,6 @@ public class ItemDetailFragment extends Fragment {
         }
         return rootView;
     }
+
+
 }
--- app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemListActivity.java
+++ app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemListActivity.java
@@ -1,5 +1,6 @@
 package com.example.yperezmartinez.examen1evaluacion;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.widget.Toast;
 
 
 /**
@@ -92,7 +94,9 @@ public class ItemListActivity extends AppCompatActivity
             // for the selected item ID.
             Intent detailIntent = new Intent(this, ItemDetailActivity.class);
             detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
+            startActivityForResult(detailIntent,1);
         }
     }
+
+
 }
