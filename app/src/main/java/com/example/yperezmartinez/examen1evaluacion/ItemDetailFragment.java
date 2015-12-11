e38b3708587e3f538911e7ee05bd3b0ef8f73976
YPerezM <yperezmartinez@danielcastelao.org>
2015-12-11 12:28:48 +0100

    Inserccion del boton del layout en el ItemDetailFragment

--- app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemDetailFragment.java
+++ app/src/main/java/com/example/yperezmartinez/examen1evaluacion/ItemDetailFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.yperezmartinez.examen1evaluacion.dummy.DummyContent;
@@ -57,11 +58,13 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
         // Show the dummy content as text in a TextView.
         if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+            //Definimos un nuevo boton, que sera el añadido al layout(R.id.button)
+            Button button = (Button) rootView.findViewById(R.id.button);
         }
 
         return rootView;
